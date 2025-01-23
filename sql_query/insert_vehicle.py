176c72b9c3778d8ed3c848939387b61c28c1518a
kqe123 <tnstn1155@naver.com>
2025-01-23 17:31:53 +0900

    자동차 현황 전처리 로직 변경!

--- sql_query/insert_vehicle.py
+++ sql_query/insert_vehicle.py
@@ -12,6 +12,7 @@ conn = pymysql.connect(
 
 cur = conn.cursor()
 file_name = r'C:/Users/tnstn/Downloads/자동차등록현황보고_자동차등록대수현황 시도별 (201101 ~ 202412).xlsx'
+
 # openpyxl 엔진을 명시적으로 지정하여 엑셀 파일 읽기
 df = pd.read_excel(file_name, header=[0, 1],skiprows=4)
 commit_interval = 1000
@@ -23,13 +24,18 @@ for i in range(len(df)) :
         indices = [-1, -5, -9, -13, -17]
         extracted = [row[col] for col in indices]
 
-        # 결측치 및 데이터 변환
-        data = [
-            int(str(val).replace(',', '')) if pd.notnull(val) and isinstance(val, str) else int(val)
-            for val in extracted
-        ]
+        # 결측치 및 데이터 변환 처리
+        data = []
+        for val in extracted:
+            if pd.notnull(val):
+                # 숫자형 문자열을 정수로 변환
+                data.append(int(str(val).replace(',', '')))
+            else:
+                # 결측치일 경우 기본값 0으로 처리
+                data.append(0)
+
         data += [row[2],row[1],row[0]]
-        
+
         sql = f"""insert vehicle_registration (total_count,special_count,truck_count
         ,van_count,sedan_count,city,state,registration_date)
         values ({data[0]},{data[1]},{data[2]},{data[3]},{data[4]},'{data[5]}','{data[6]}','{data[7]}')"""
